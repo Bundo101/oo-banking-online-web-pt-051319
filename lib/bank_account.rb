fa371100db3d12b6bb096ce6d7892228a4c5d628
James Britton <brittonjp@gmail.com>
2019-06-10 16:57:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
   
-  attr_reader :balance, :name
-  attr_accessor :status
+  attr_reader :name
+  attr_accessor :balance, :status
   
   def initialize(name)
     @name = name
@@ -9,5 +9,8 @@ class BankAccount
     @status = "open"
   end
   
+  def deposit(amount)
+    @balance += amount
+  end  
   
 end
